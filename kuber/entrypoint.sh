957a5fd7d82ce1455ac97e42b5c6a1ea7dc6b069
Eric Charles <eric@datalayer.io>
2018-01-01 12:46:18 +0100

    kuber take inscure address param

--- kuber/entrypoint.sh
+++ kuber/entrypoint.sh
@@ -3,13 +3,14 @@
 /opt/kuber/kuber \
   server \
   --apiserver-host=$APISERVER_HOST \
-  --kuber-rest=$KUBER_REST \
-  --kuber-ws=$KUBER_WS \
   --azure-application-id=$AZURE_APPLICATION_ID \
   --azure-redirect=$AZURE_REDIRECT \
   --azure-scope=$AZURE_SCOPE \
+  --hdfs=$HDFS \
+  --insecure-bind-address=$KUBER_INSECURE_BIND_ADDRESS \
+  --kuber-plane=$KUBER_PLANE \
+  --kuber-rest=$KUBER_REST \
+  --kuber-ws=$KUBER_WS \
   --spitfire-rest=$SPITFIRE_REST \
   --spitfire-ws=$SPITFIRE_WS \
-  --hdfs=$HDFS \
-  --twitter-redirect=$TWITTER_REDIRECT \
-  --kuber-plane=$KUBER_PLANE
+  --twitter-redirect=$TWITTER_REDIRECT
