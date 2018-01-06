d0cd629359683a1e2e41347d28cfbfd680300ee6
Eric Charles <eric@datalayer.io>
2018-01-06 19:49:28 +0100

    microsoft conf for kuber-plane

--- kuber-plane/entrypoint.sh
+++ kuber-plane/entrypoint.sh
@@ -5,14 +5,15 @@ cd /opt/kuber
 ./kuber \
   server \
   --apiserver-host=$APISERVER_HOST \
-  --microsoft-application-id=$AZURE_APPLICATION_ID \
-  --microsoft-redirect=$AZURE_REDIRECT \
-  --microsoft-scope=$AZURE_SCOPE \
   --hdfs=$HDFS \
   --insecure-bind-address=$KUBER_INSECURE_BIND_ADDRESS \
   --kuber-plane=$KUBER_PLANE \
   --kuber-rest=$KUBER_REST \
   --kuber-ws=$KUBER_WS \
+  --microsoft-application-id=$MICROSOFT_APPLICATION_ID \
+  --microsoft-redirect=$MICROSOFT_REDIRECT \
+  --microsoft-secret=$MICROSOFT_SECRET \
+  --microsoft-scope=$MICROSOFT_SCOPE \
   --spitfire-rest=$SPITFIRE_REST \
   --spitfire-ws=$SPITFIRE_WS \
   --twitter-redirect=$TWITTER_REDIRECT
