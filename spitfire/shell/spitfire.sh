f7d9c1130a21927dc34667be3acd0165a38cd398
Eric Charles <eric@datalayer.io>
2018-01-15 14:32:09 +0100

    spitfire to use ZEPPELIN_CONF_DIR and clone a repo for notes and config

--- spitfire/shell/spitfire.sh
+++ spitfire/shell/spitfire.sh
@@ -17,10 +17,28 @@
 # specific language governing permissions and limitations
 # under the License.
 
+# rm -fr /opt/spitfire/notebook
+git clone $SPITFIRE_NOTEBOOK_REPO /opt/spitfire/notebook
+
+cat << EOF > /opt/spitfire/notebook/.git/hooks/pre-commit
+#!/bin/sh
+git add -A
+EOF
+chmod +x /opt/spitfire/notebook/.git/hooks/pre-commit
+cat << EOF > /opt/spitfire/notebook/.git/hooks/post-commit
+#!/bin/sh
+git push origin master -f -u
+EOF
+chmod +x /opt/spitfire/notebook/.git/hooks/post-commit
+
+# cp /opt/spitfire/notebook/_conf/* /opt/spitfire/conf/
+# sed -i s/INFO/DEBUG/ /opt/spitfire/conf/log4j.properties
+# cat /opt/spitfire/conf/users >> /opt/spitfire/conf/shiro.ini
+
 POD_NAME=$HOSTNAME
-sed -e "s|SPARK_KUBERNETES_DRIVER_POD_NAME|$POD_NAME|g" /opt/spitfire/conf/interpreter.json.template > /tmp/interpreter-tmp.json
+sed -e "s|SPARK_KUBERNETES_DRIVER_POD_NAME|$POD_NAME|g" /opt/spitfire/notebook/_lib/interpreter.json.template > /tmp/interpreter-tmp.json
 
 RESOURCESTAGINGSERVER_IP=$(kubectl get svc spark-k8s-resource-staging-service -o jsonpath='{.spec.clusterIP}')
-sed -e "s|SPARK_KUBERNETES_RESOURCESTAGINGSERVER_URI|$RESOURCESTAGINGSERVER_IP|g" /tmp/interpreter-tmp.json > /opt/spitfire/conf/interpreter.json
+sed -e "s|SPARK_KUBERNETES_RESOURCESTAGINGSERVER_URI|$RESOURCESTAGINGSERVER_IP|g" /tmp/interpreter-tmp.json > /opt/spitfire/notebook/_lib/interpreter.json
 
 /opt/spitfire/bin/zeppelin.sh "$@"
