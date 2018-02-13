775f404669d940ae822fe5e73dfc7f89dc2d868f
Eric Charles <eric@datalayer.io>
2018-02-13 14:44:18 +0100

    explorer

--- explorer/entrypoint.sh
+++ explorer/entrypoint.sh
@@ -5,22 +5,22 @@ cd /opt/kuber
 ./kuber \
   server \
   --apiserver-host="$APISERVER_HOST" \
-  --hdfs="$HDFS" \
-  --insecure-bind-address="$KUBER_INSECURE_BIND_ADDRESS" \
-  --kuber-board="$KUBER_BOARD" \
-  --kuber-rest="$KUBER_REST" \
-  --kuber-ws="$KUBER_WS" \
   --google-api-key="$GOOGLE_API_KEY" \
   --google-client-id="$GOOGLE_CLIENT_ID" \
   --google-redirect="$GOOGLE_REDIRECT" \
   --google-secret="$GOOGLE_SECRET" \
   --google-scope="$GOOGLE_SCOPE" \
+  --hdfs="$HDFS" \
+  --insecure-bind-address="$KUBER_INSECURE_BIND_ADDRESS" \
+  --kuber-board="$KUBER_BOARD" \
+  --kuber-rest="$KUBER_REST" \
+  --kuber-ws="$KUBER_WS" \
   --microsoft-application-id="$MICROSOFT_APPLICATION_ID" \
   --microsoft-redirect="$MICROSOFT_REDIRECT" \
   --microsoft-secret="$MICROSOFT_SECRET" \
   --microsoft-scope="$MICROSOFT_SCOPE" \
   --spitfire-rest="$SPITFIRE_REST" \
   --spitfire-ws="$SPITFIRE_WS" \
-  --twitter-consumer-key="$TWITTER_CONSUMER_KEY"
-  --twitter-consumer-secret="$TWITTER_CONSUMER_SECRET"
+  --twitter-consumer-key="$TWITTER_CONSUMER_KEY" \
+  --twitter-consumer-secret="$TWITTER_CONSUMER_SECRET" \
   --twitter-redirect="$TWITTER_REDIRECT"
