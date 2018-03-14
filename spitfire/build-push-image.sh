d7e282249c223ed84c2ddf14c087ae0be0ff3efc
Eric Charles <eric@datalayer.io>
2018-03-14 09:24:55 +0100

    spitfire build script

---
+++ spitfire/build-push-image.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Licensed to Datalayer (http://datalayer.io) under one or more
+# contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. Datalayer licenses this file
+# to you under the Apache License, Version 2.0 (the 
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+cd /dla/docker-files/spitfire
+docker build \
+  --squash \
+  --compress \
+  -t datalayer/spitfire:0.0.1 \
+  .
+docker push datalayer/spitfire:0.0.1
+
+# docker run --network host --name spitfire-local -v /root/.kube:/home/datalayer/.kube -it --rm --env APISERVER_HOST=http://localhost:8001 --env SPITFIRE_NOTEBOOK_REPO=https://github.com/datalayer/notebook-init.git --env ZEPPELIN_CONF_DIR=/opt/spitfire/notebook/_conf datalayer/spitfire:0.0.1
